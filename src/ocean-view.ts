4b8d09d78d7345e3bb1befc213547df1facbecd4
Scott Garner <scott@j38.net>
2025-02-04 21:43:03 -0800

    Updates.

--- src/ocean-view.ts
+++ src/ocean-view.ts
@@ -147,7 +147,7 @@ export class OceanView {
     const roll = THREE.MathUtils.degToRad(data.roll);
 
     const quaternion = new THREE.Quaternion();
-    quaternion.setFromEuler(new THREE.Euler(-pitch, yaw, roll, "XYZ"));
+    quaternion.setFromEuler(new THREE.Euler(pitch, yaw, roll, "XYZ"));
 
     this.camera.quaternion.copy(quaternion);
   }
